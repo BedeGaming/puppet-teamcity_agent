67b2249d0226a66e39210f343ac79133aedce75a
Toby Brain <tobyb@freshview.com>
2015-02-13 10:07:51 +1100

    buildAgent.properties must be writable.
    This module relies on the build agent setting the authorizationToken which requires writing out the properties file, however it's possible that this properties file is not writable by the agent user. Best to capture this requirement within the module itself.

--- manifests/config.pp
+++ manifests/config.pp
@@ -8,6 +8,7 @@ class teamcity_agent::config inherits teamcity_agent {
     replace => 'no',
     source  => 'puppet:///modules/teamcity_agent/buildAgent.properties',
     owner   => $user,
+    mode    => 0644
   }
 
   # Have the latest properties augeas lens available for configuring properties files
